40022042fd3b1f86de1fda20d075189a155c52ed
guaracyalimam <guaracyaraujolima@gmail.com>
2018-08-29 14:29:14 -0300

    Implementa a AUTORIZACAO com PasheListener

--- src/br/com/caelum/livraria/bean/LoginBean.java
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -2,6 +2,7 @@ package br.com.caelum.livraria.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -21,8 +22,13 @@ public class LoginBean {
 				+ this.usuario.getEmail());
 
 		boolean existe = new UsuarioDao().existe(this.usuario);
+		
+		FacesContext context = FacesContext.getCurrentInstance();
 
 	    if (existe) {
+	    	context.getExternalContext()
+	    			.getSessionMap()
+	    			.put("usuarioLogado", this.usuario); //salva a seção do usuario logado 
 	        return "livro?faces-redirect=true";
 	    }
 		
